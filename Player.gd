008d686e6bf4905881bf870e9dabbd96c7fbccc6
Adam Britt <adam@adambritt.com>
2022-07-28 19:05:35 +0200

    after day 8

--- Player.gd
+++ Player.gd
@@ -1,47 +1,55 @@
 extends KinematicBody2D
+class_name Player
 
-export(int) var JUMP_FORCE = -130
-export(int) var JUMP_RELEASE_FORCE = -70
-export(int) var MAX_SPEED = 50
-export(int) var ACCELERATION = 10
-export(int) var FRICTION = 10
-export(int) var GRAVITY = 4
-export(int) var ADDITIONAL_FALL_GRAVITY = 4
+enum {
+	MOVE,
+	CLIMB
+}
+
+export(Resource) var moveData
 
 var velocity = Vector2.ZERO
+var state = MOVE
 
 onready var animatedSprite = $AnimatedSprite
+onready var ladderCheck = $LadderCheck
 
 func _ready():
 	animatedSprite.frames = load("res://PlayerRedSkin.tres")
-	
+
 
 func _physics_process(delta):	
-	apply_gravity()
 	var input = Vector2.ZERO
-	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input.x = Input.get_axis("ui_left", "ui_right")
+	input.y = Input.get_axis("ui_up", "ui_down")
 	
+	match state:
+		MOVE: move_state(input)
+		CLIMB: climb_state(input)
+		
+func move_state(input):	
+	if is_on_ladder() and Input.is_action_pressed("ui_up"): state = CLIMB
+		
+	apply_gravity()
 	if input.x == 0:
 		apply_friction()
 		animatedSprite.animation = "Idle"
 	else:
 		apply_acceleration(input.x)
 		animatedSprite.animation = "Run"
-		if input.x > 0:
-			animatedSprite.flip_h = true
-		elif input.x < 0:
-			animatedSprite.flip_h = false
+		
+		animatedSprite.flip_h = input.x > 0
 	
 
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
-			velocity.y = JUMP_FORCE
+			velocity.y = moveData.JUMP_FORCE
 	else:
 		animatedSprite.animation = "Jump"
-		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
-			velocity.y = JUMP_RELEASE_FORCE
+		if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
+			velocity.y = moveData.JUMP_RELEASE_FORCE
 		if velocity.y > 0:
-			velocity.y += ADDITIONAL_FALL_GRAVITY
+			velocity.y += moveData.ADDITIONAL_FALL_GRAVITY
 			
 	var was_in_air = not is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -49,12 +57,28 @@ func _physics_process(delta):
 	if just_landed:
 		animatedSprite.animation = "Run"
 		animatedSprite.frame = 1
+
+func climb_state(input):
+	if not is_on_ladder(): state = MOVE
+	if input.length() != 0:
+		animatedSprite.animation = "Run"
+	else:
+		animatedSprite.animation = "Idle"
+	velocity = input * 50
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func is_on_ladder():
+	if not ladderCheck.is_colliding(): return false
+	var collider = ladderCheck.get_collider()
+	if not collider is Ladder: return false
+	return true
 	
 func apply_gravity():
-	velocity.y += GRAVITY	
+	velocity.y += moveData.GRAVITY
+	velocity.y = min(velocity.y, 300)	
 
 func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, FRICTION)
+	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
 	
 func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
+	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
